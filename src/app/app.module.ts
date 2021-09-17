349535d3a8a692bc486507cffdef8c021cc6fc20
Karim-mo <karim.mohamed420@gmail.com>
2021-09-17 16:48:51 +0200

    setup basic components and views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { RestaurantsPageComponent } from './views/restaurants-page/restaurants-page.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    RestaurantsPageComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule
